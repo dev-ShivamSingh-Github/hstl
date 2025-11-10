d394d9ed702978a91af79f75f9a1dee63ea857df
Shivam Singh <shivamsinghghost@gmail.com>
2025-11-10 22:23:02 +0530

    Renamed .env to env as it now contain 'DJANGO's SECRET_KEY' in seprate module and can be imported into settings.py. Started working with forms and redirect and session.

--- student/urls.py
+++ student/urls.py
@@ -4,4 +4,9 @@ from . import views
 app_name = 'student'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home', views.home, name='home'),
 ]
+
+# i can have only one page based web app...
+# in form's action the url can be named url 
+# {% url 'studindex' %}
--- student/views.py
+++ student/views.py
@@ -1,7 +1,22 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NameForm
 
 # Create your views here.
 def index(request):
-    content = None
+    content = {}
+    if request.method == 'POST':
+        content = NameForm(request.POST)
+        if content.is_valid():
+            request.session['form'] = content.cleaned_data
+            return redirect('student:home')
+    else:
+        content['form'] = NameForm()
     return render(request, 'student/index.html', content)
+
+def home(request):
+    content = {}
+    content['form'] = request.session.get('form')
+    if content['form'] and False:
+        return render(request, 'student/home.html', content)
+    else:
+        return redirect('student:index')
